b53f0ae7e55b99bf8cc42c71bdb9ac9ad99c8e44
Manish Devgan <manish.nsit8@gmail.com>
2017-06-05 16:56:41 +0530

    updated

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-/*
-  @Author : Manish Devgan
-  @Github : https://github.com/gabru-md
-  @Language : Javascript
-*/
-
 var http = require('http');
 var express = require('express');
 var fs = require('fs');
 var app = express();
-
 var shortid = require('shortid');
 
 //console.log(shortid.generate());
@@ -19,18 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
-
-/*
-  Simple test to create a server
-*/
-
-var server = http.createServer(function(req,res){
-  res.writeHead(200,{'Content-Type':'text/plain'});
-  res.end('Hey There!');
-});
-
-server.listen(5000,'127.0.0.1');
-// server working on port 5000 of localhost
+app.set('view engine','ejs');
 
 console.log('Working now!')
 
@@ -40,23 +22,16 @@ app.get('/short',function(req,res){
   var myInputStream = fs.createReadStream(__dirname + "/shortner.html");
   myInputStream.pipe(res);
 });
-
-app.post('/url',function(req,res){
-  res.writeHead(200,{'Content-Type':'text/plain'});
+app.post('/short/url',function(req,res){
+  //res.writeHead(200,{'Content-Type':'text/plain'});
   //res.end('URL is : ' + req.body.mainurl_); URL as entered by the user
-  res.end("localhost:5000/" + shortid.generate()); // generates unique ID and shortens the url.
-  console.log(req.body.mainurl);
+  var uid = shortid.generate();
+  console.log(uid);
+  uid = "localhost:5000/" + uid;
+  res.render('uid',{uniqueid: uid});
+  res.end("localhost:5000/" + uid); // generates unique ID and shortens the url.
+  //console.log(req.body.mainurl);
 });
-
-/*
-  Funcion below will be the 
-  redirector function for 
-  out URL-Shortner.
-  
-  Below is a temporary snippet. 
-  It will be replaced as soon as 
-  mongoDB is connected to it.
-*/
 
 app.get('/:uid',function(req,res){
   req.url="google.co.in";
@@ -66,5 +41,4 @@ app.get('/:uid',function(req,res){
   res.end();
   console.log('Redirector');
 });
-
 app.listen(5000);
