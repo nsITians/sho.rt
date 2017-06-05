557ee6fe9353aa3f38ae91af208e4fe97f2ed195
Manish Devgan <manish.nsit8@gmail.com>
2017-06-05 14:16:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+/*
+  @Author : Manish Devgan
+  @Github : https://github.com/gabru-md
+  @Language : Javascript
+*/
+
 var http = require('http');
 var express = require('express');
 var fs = require('fs');
 var app = express();
+
 var shortid = require('shortid');
 
 //console.log(shortid.generate());
@@ -12,12 +19,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
+
+/*
+  Simple test to create a server
+*/
+
 var server = http.createServer(function(req,res){
   res.writeHead(200,{'Content-Type':'text/plain'});
-  res.end('Hey ninjas');
+  res.end('Hey There!');
 });
 
 server.listen(5000,'127.0.0.1');
+// server working on port 5000 of localhost
+
 console.log('Working now!')
 
 app.get('/short',function(req,res){
@@ -26,10 +40,31 @@ app.get('/short',function(req,res){
   var myInputStream = fs.createReadStream(__dirname + "/shortner.html");
   myInputStream.pipe(res);
 });
+
 app.post('/url',function(req,res){
   res.writeHead(200,{'Content-Type':'text/plain'});
   //res.end('URL is : ' + req.body.mainurl_); URL as entered by the user
   res.end("localhost:5000/" + shortid.generate()); // generates unique ID and shortens the url.
   console.log(req.body.mainurl);
 });
+
+/*
+  Funcion below will be the 
+  redirector function for 
+  out URL-Shortner.
+  
+  Below is a temporary snippet. 
+  It will be replaced as soon as 
+  mongoDB is connected to it.
+*/
+
+app.get('/:uid',function(req,res){
+  req.url="google.co.in";
+  res.writeHead(301,
+  {Location: 'https://google.co.in'}
+  );
+  res.end();
+  console.log('Redirector');
+});
+
 app.listen(5000);
