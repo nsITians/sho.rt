f4a1f53924cf17222dfbe6d4ba6fdc24a4c8e64b
Manish Devgan <manish.nsit8@gmail.com>
2017-06-05 15:08:24 +0530

    Update re.js

--- init/re.js
+++ init/re.js
@@ -3,12 +3,24 @@
   @Github : https://github.com/gabru-md
   @Language : Javascript
 */
+var express = require('express')
 
+var app = express();
 // A very simple Redirector in JS
+
+/*
+UID is a parameter that will
+determine the ID of hash.
+
+This ID will be used to point
+to the actual URL in mongoDB.
+*/
+
 app.get('/:uid',function(req,res){
   req.url="google.co.in";
   res.writeHead(301,
-  {Location: 'https://google.co.in'}
+  {
+    Location: 'https://google.co.in'}
   );
   res.end();
   console.log('Redirector');
